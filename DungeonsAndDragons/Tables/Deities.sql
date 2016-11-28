c03571db92a2c372fa9dd647052de22f7d5fd116
dangerworm <dangerworm@gmail.com>
2016-11-28 23:29:56 +0000

    Shifted the schema round a little to match the book.

---
+++ DungeonsAndDragons/Tables/Deities.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[Deities]
+(
+    DeityId INT IDENTITY(1,1) 
+		CONSTRAINT Deities_DeityId_PK PRIMARY KEY,
+    AuthorityLevelId INT 
+		CONSTRAINT Deities_AuthorityLevelId_FK FOREIGN KEY REFERENCES dbo.AuthorityLevels(AuthorityLevelId),
+    CompassionLevelId INT 
+		CONSTRAINT Deities_CompassionLevelId_FK FOREIGN KEY REFERENCES dbo.CompassionLevels(CompassionLevelId),
+    Name VARCHAR(50) NOT NULL,
+    FlavourText VARCHAR(MAX)
+);
---
+++ DungeonsAndDragons/Tables/Enums/AuthorityLevels.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [dbo].[AuthorityLevels]
+(
+    AuthorityLevelId INT IDENTITY(1,1) 
+		CONSTRAINT AuthorityLevels_AuthorityLevelId_PK PRIMARY KEY,
+    Name VARCHAR(50) NOT NULL
+);
