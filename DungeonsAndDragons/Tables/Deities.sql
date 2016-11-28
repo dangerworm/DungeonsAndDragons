8a7f9cbf88e35c653a5095078d48aa8a8156f0e0
dangerworm <dangerworm@gmail.com>
2016-11-28 23:39:00 +0000

    Added the last of the tables (for now).

--- DungeonsAndDragons/Tables/Deities.sql
+++ DungeonsAndDragons/Tables/Deities.sql
@@ -6,5 +6,6 @@
 		CONSTRAINT Deities_AuthorityLevelId_FK FOREIGN KEY REFERENCES dbo.AuthorityLevels(AuthorityLevelId),
     CompassionLevelId INT 
 		CONSTRAINT Deities_CompassionLevelId_FK FOREIGN KEY REFERENCES dbo.CompassionLevels(CompassionLevelId),
+	Name VARCHAR(50) NOT NULL,
     FlavourText VARCHAR(MAX)
 );
