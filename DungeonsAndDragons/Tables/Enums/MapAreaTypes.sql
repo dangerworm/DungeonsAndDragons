beaf9dd391701aa241944170e201761eb64d704d
Drew Morgan <drew.morgan@paystream.co.uk>
2016-12-07 12:44:37 +0000

    Added map areas and shops.

---
+++ DungeonsAndDragons/Tables/Enums/MapAreaTypes.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [dbo].[MapAreaTypes]
+(
+    MapAreaTypeId INT IDENTITY(1,1) 
+		CONSTRAINT MapAreaTypes_MapAreaTypeId_PK PRIMARY KEY,
+    Name VARCHAR(50) NOT NULL
+);
---
+++ DungeonsAndDragons/Tables/Shops.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE [dbo].[Shops]
+(
+    ShopId INT IDENTITY(1,1) 
+		CONSTRAINT Shops_ShopId_PK PRIMARY KEY,
+    Name VARCHAR(50) NOT NULL,
+    OwnerActorId INT NOT NULL 
+		CONSTRAINT Shops_OwnerActorId_FK FOREIGN KEY REFERENCES dbo.Actors(ActorId),
+    CostModifier DECIMAL NOT NULL 
+		CONSTRAINT Shops_CostModifier_DF DEFAULT(0),
+    SellsMagicItems BIT NOT NULL 
+		CONSTRAINT Shops_SellsMagicItems_DF DEFAULT(0),
+    FlavourText VARCHAR(MAX)
+);
