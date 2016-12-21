2bc90c3084bd1e99779dc5de3559d4fd9527f7ca
FERMI\Drew <dangerworm@gmail.com>
2016-12-21 20:42:03 +0000

    Added validation messages to player character form

--- DungeonsAndDragons/Models/PlayerCharacterModel.cs
+++ DungeonsAndDragons/Models/PlayerCharacterModel.cs
@@ -10,21 +10,20 @@ namespace DungeonsAndDragons.Models
         public int ActorId { get; set; }
         [Required(ErrorMessage = "Please enter a name for your character")]
         public string Name { get; set; }
-        [NotNull]
+        [Required(ErrorMessage = "Please select your character's class")]
         public Classes ClassId { get; set; }
         public int? ParagonPathId { get; set; }
         public int? EpicDestinyId { get; set; }
         public int ExperiencePoints { get; set; }
-        [NotNull]
+        [Required(ErrorMessage = "Please select your character's race")]
         public Races RaceId { get; set; }
-        [NotNull]
-        //[Range(1, int.MaxValue, ErrorMessage = "Please enter a value bigger than {1}")]
+        [Range(16, int.MaxValue, ErrorMessage = "Please enter a value bigger than {1}")]
         public int Age { get; set; }
-        [NotNull]
+        [Required(ErrorMessage = "Please select your character's gender")]
         public Genders GenderId { get; set; }
         public string Height { get; set; }
         public int WeightPounds { get; set; }
-        [NotNull]
+        [Required(ErrorMessage = "Please select your character's alignment")]
         public Alignments AlignmentId { get; set; }
         public Deities? DeityId { get; set; }
 
