5181d33178cb774a3743c5fbe9aa9501d7d7164b
FERMI\Drew <dangerworm@gmail.com>
2016-12-21 21:10:50 +0000

    Added field names to viewmodel

--- DungeonsAndDragons/Models/PlayerCharacterModel.cs
+++ DungeonsAndDragons/Models/PlayerCharacterModel.cs
@@ -1,5 +1,4 @@
 ﻿using System.ComponentModel.DataAnnotations;
-using DungeonsAndDragons_Data;
 using DungeonsAndDragons_Data.Enums;
 
 namespace DungeonsAndDragons.Models
@@ -7,24 +6,47 @@ namespace DungeonsAndDragons.Models
     public class PlayerCharacterModel
     {
         public int? Id { get; set; }
+
         public int ActorId { get; set; }
+
         [Required(ErrorMessage = "Please enter a name for your character")]
         public string Name { get; set; }
+
+        [Display(Name="Class")]
         [Required(ErrorMessage = "Please select your character's class")]
         public Classes ClassId { get; set; }
+
+        [Display(Name="Paragon Path")]
         public int? ParagonPathId { get; set; }
+
+        [Display(Name="Epic Destiny")]
         public int? EpicDestinyId { get; set; }
+
+        [Display(Name="Experience Points (XP)")]
         public int ExperiencePoints { get; set; }
+
+        [Display(Name="Race")]
         [Required(ErrorMessage = "Please select your character's race")]
         public Races RaceId { get; set; }
+
+        [Required(ErrorMessage = "Please enter your character's age")]
         [Range(16, int.MaxValue, ErrorMessage = "Please enter a value bigger than {1}")]
         public int Age { get; set; }
+
+        [Display(Name="Gender")]
         [Required(ErrorMessage = "Please select your character's gender")]
         public Genders GenderId { get; set; }
+
         public string Height { get; set; }
+
+        [Display(Name="Weight (lb)")]
         public int WeightPounds { get; set; }
+
+        [Display(Name="Alignment")]
         [Required(ErrorMessage = "Please select your character's alignment")]
         public Alignments AlignmentId { get; set; }
+
+        [Display(Name="Deity")]
         public Deities? DeityId { get; set; }
 
         public PlayerCharacterModel()
